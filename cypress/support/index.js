5c54746813b0f0efb178b0acf635528b00078e10
Beth Shook <beth.shook@gmail.com>
2019-10-18 18:18:05 -0500

    Add before hook to Cypress support file

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(function() {
+  cy.visit(Cypress.config().baseUrl); // Hook that runs before all tests in the block
+});
