38d435cc3c04172df402ae4de33759066a8de945
Eric Pereyra <eric@nolte.io>
2020-06-22 06:31:27 -0500

    Add lighthouse tests

---
+++ tests/lighthouse/evaluate.py
@@ -0,0 +1,24 @@
+import sys
+import json
+
+exit_code = 0
+
+TARGETS = {}
+TARGETS['performance'] = .7
+TARGETS['seo'] = .8
+TARGETS['accessibility'] = .8
+TARGETS['best-practices'] = .8
+
+with open('tests/lighthouse/reports/report.json') as f:
+  report = json.load(f)
+
+for key in TARGETS:
+   if report['categories'][key]['score'] < TARGETS[key]:
+      print('Failed: The {} score of {} was less than the target of {}'
+         .format(key, report['categories'][key]['score'], TARGETS[key]))
+      exit_code = 1
+   else:
+      print('Passed: The {} score of {} was good enough :-)'
+         .format(key, report['categories'][key]['score']))
+
+sys.exit(exit_code)
